604526232da0b2bb1274745c473a4e31d16719f1
Lilyennd <victoriariffo21@gmail.com>
2026-05-27 15:19:09 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/PlanesDeVuelos/PlanesDeVuelosApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.PlanesDeVuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanesDeVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanesDeVuelosApplication.class, args);
+	}
+
+}
