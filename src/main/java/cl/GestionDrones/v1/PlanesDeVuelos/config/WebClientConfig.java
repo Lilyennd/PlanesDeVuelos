2c00f336502ba302ec5cd6a30d8d6a34d16279cf
Lilyennd <victoriariffo21@gmail.com>
2026-07-02 13:51:14 -0400

    webconfig v1.2

--- src/main/java/cl/GestionDrones/v1/PlanesDeVuelos/config/WebClientConfig.java
+++ src/main/java/cl/GestionDrones/v1/PlanesDeVuelos/config/WebClientConfig.java
@@ -1,11 +1,24 @@
 package cl.GestionDrones.v1.PlanesDeVuelos.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${api.url.pilotos}")
+    private String pilotosUrl;
+
+    @Value("${api.url.aeronaves}")
+    private String aeronavesUrl;
+
+    @Value("${api.url.zonasrestringidas}")
+    private String zonasUrl;
+
+    @Value("${api.url.notificaciones}")
+    private String notificacionesUrl;
 
     @Bean
     public WebClient.Builder webClientBuilder() {
@@ -14,24 +27,21 @@ public class WebClientConfig {
     
     @Bean
     public WebClient pilotosWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081/api/v1/pilotos").build();
+        return builder.baseUrl(pilotosUrl).build();
     }
-
 
     @Bean
     public WebClient aeronavesWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8082/api/v1/aeronaves").build();
+        return builder.baseUrl(aeronavesUrl).build();
     }
-
 
     @Bean
     public WebClient zonasWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8090/api/v1/ZonasRestringidas").build();
+        return builder.baseUrl(zonasUrl).build();
     }
-
   
     @Bean
     public WebClient notificacionesWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8091/api/v1/notificaciones").build();
+        return builder.baseUrl(notificacionesUrl).build();
     }
 }
